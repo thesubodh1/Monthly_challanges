0192efb47dc4e9929951c0bf366553504e47d020
thesubodh1 <thesubodh1@gmail.com>
2024-05-11 18:22:49 +0545

    redirection used for numbered months

--- challanges/urls.py
+++ challanges/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 # urlpatterns is a list
 urlpatterns = [
-    path("<month>",views.monthly_challanges)
+    path("<int:month>",views.monthly_challange_number),
+    path("<str:month>",views.monthly_challanges)
 ]
--- challanges/views.py
+++ challanges/views.py
@@ -1,14 +1,42 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound,HttpResponseRedirect
 
-# Create your views here.
+monthly_challange = {
+    "january" : "learn Python",
+    "february" : "learn Django",
+    "march" : "learn react",
+    "april" : "learn html",
+    "may" : "learn css",
+    'june' : "learn git",
+    'july' : "jog for 20km",
+    "august" : "loose 2 kg",
+    "september" : "walk for 20 minutes",
+    "october" : "break",
+    "november" : "start a new job",
+    "december" : "compleate if any pending"
+}
+
+# Create your views here. it also works
+# def monthly_challange_dict(request,month):
+#     if month in monthly_challanges:
+#         return HttpResponse(monthly_challanges[month])
+#     else:
+#         return HttpResponseNotFound("this month doesnt exist")
+
+def monthly_challange_number(request, month):
+    months = list(monthly_challange.keys())
+    try:
+        redirect_month=months[month-1]
+    except IndexError:
+        return HttpResponseNotFound("invalid month")
+    else:
+        return HttpResponseRedirect("/challanges/"+ redirect_month)
 
 def monthly_challanges(request,month):
-    challanges = None
-    if month == "january":
-        challanges = "compleate Python"
-    elif month == "febuary":
-        challanges = "compleate Django"
-    else:
+    try:
+        challanges = monthly_challange[month]
+    except:
         return HttpResponseNotFound("This month is not supported!!")
-    return HttpResponse(challanges)
+    else:
+        return HttpResponse(challanges)
+    

a4e04c4cc5a288e3e1a82e6b5293691525dded43
thesubodh1 <thesubodh1@gmail.com>
2024-05-11 17:18:40 +0545

    made it dynamic

--- challanges/urls.py
+++ challanges/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 # urlpatterns is a list
 urlpatterns = [
-    path("january",views.january),
-    path("february",views.february)
+    path("<month>",views.monthly_challanges)
 ]
--- challanges/views.py
+++ challanges/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-def january (request):
-    return HttpResponse("Compleate Python")
-
-def february(request):
-    return HttpResponse("compleate django")
+def monthly_challanges(request,month):
+    challanges = None
+    if month == "january":
+        challanges = "compleate Python"
+    elif month == "febuary":
+        challanges = "compleate Django"
+    else:
+        return HttpResponseNotFound("This month is not supported!!")
+    return HttpResponse(challanges)

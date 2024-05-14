a87f29f740b33435c3187078cd57768e6def3d29
thesubodh1 <thesubodh1@gmail.com>
2024-05-14 20:32:28 +0545

    nav bar created by using new include tag

--- challanges/urls.py
+++ challanges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 # urlpatterns is a list
 urlpatterns = [
-    path("",views.landing),
+    path("",views.landing, name="nav-bar"),
     path("<int:month>",views.monthly_challange_number),
     path("<str:month>",views.monthly_challanges, name="month-challanges")
 ]
